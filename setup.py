b13c5ded34a09816b2a16dcf8650ba76ac4bcea5
Éric Massé <Ericmas001@gmail.com>
2017-06-11 10:05:27 -0400

    no more dash

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup, find_packages
 
 # Get __version__ which is stored in src/version.py
-ver_file = os.path.join('py-userbase', 'version.py')
+ver_file = os.path.join('py_userbase', 'version.py')
 exec(open(ver_file).read())
 
 setup(
-    name='py-userbase',
+    name='py_userbase',
     version=__version__,
     packages=find_packages(),
-    namespace_packages=['py-userbase'],
+    namespace_packages=['py_userbase'],
     url='',
     license='',
     author='ericmas001',
