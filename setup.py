e722bbc097a979d552e9fe43dd43cde33bcf3340
Ericmas001 <ericmas001@gmail.com>
2017-06-11 09:56:28 -0400

    corrections

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import os
 from setuptools import setup, find_packages
 
 # Get __version__ which is stored in src/version.py
-ver_file = os.path.join('src', 'version.py')
+ver_file = os.path.join('py-userbase', 'version.py')
 exec(open(ver_file).read())
 
 setup(
-    name='com.ericmas001.py-userbase',
+    name='py-userbase',
     version=__version__,
     packages=find_packages(),
     namespace_packages=['py-userbase'],
